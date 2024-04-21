0a0720672cea6b7fdb9ecb94d0958e87d2bb55cb
BYUMVUHOREAimable <aimablebyumvuhore@gmail.com>
2024-04-21 18:11:28 +0200

    String to char array

---
+++ src/bai/stringtochararray/StringToCharArray.java
@@ -0,0 +1,15 @@
+package bai.stringtochararray;
+
+import java.util.Arrays;
+
+public class StringToCharArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="Java is programing language";
+		char[] chars=str.toCharArray();
+		System.out.println(Arrays.toString(chars));
+
+	}
+
+}
